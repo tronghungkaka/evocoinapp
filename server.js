d3e0f811f87d0b5c18a0d941de5a501e61424a1f
thangnh <thang.nguyenhuu91@gmail.com>
2018-03-21 09:22:56 +0700

    retry to fix that shit bug

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 //Install express server
 const express = require('express');
 const app = express();
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
+//     // intercept OPTIONS method
+//     if ('OPTIONS' == req.method) {
+//       res.send(200);
+//     }
+//     else {
+//       next();
+//     }
+// };
 
-app.use(allowCrossDomain);
+// app.use(allowCrossDomain);
+
+var cors = require('cors');
+app.use(cors());
+
 //Serve only static files from dist directory
 app.use(express.static(__dirname + '/dist'));
 
