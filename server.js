9f37762086f57b9a1924391b3ccbbc5dc30ba41e
thangnh <thang.nguyenhuu91@gmail.com>
2018-03-21 09:19:53 +0700

    add allow cross domain in server.js

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 //Install express server
 const express = require('express');
 const app = express();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
 //Serve only static files from dist directory
 app.use(express.static(__dirname + '/dist'));
 
