4190259e48569573b4e656e9d86de54654b699f4
thangnh <thang.nguyenhuu91@gmail.com>
2018-03-21 11:32:00 +0700

    try to use http-proxy again

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 //Install express server
 const express = require('express');
 const app = express();
-// var allowCrossDomain = function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-//     // intercept OPTIONS method
-//     if ('OPTIONS' == req.method) {
-//       res.send(200);
-//     }
-//     else {
-//       next();
-//     }
-// };
+var proxy = httpProxy.createProxyServer({});
 
-// app.use(allowCrossDomain);
+function apiProxy(host, port) {
+  return function(req, res, next) {
+    if(req.url.match(new RegExp('^\/api\/'))) {
+      proxy.proxyRequest(req, res, {host: host, port: port});
+    } else {
+      next();
+    }
+  }
+}
 
 var cors = require('cors');
 app.use(cors());
+
+app.use(apiProxy('localhost', 3000));
 
 //Serve only static files from dist directory
 app.use(express.static(__dirname + '/dist'));
