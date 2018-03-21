03d72fb69949f20cfbea95f6442cec9255e50006
thangnh <thang.nguyenhuu91@gmail.com>
2018-03-21 11:07:17 +0700

    remove http-proxy config in server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 //Install express server
 const express = require('express');
 const app = express();
-var httpProxy = require('http-proxy');
-var proxy = new httpProxy.RoutingProxy();
-
 // var allowCrossDomain = function(req, res, next) {
 //     res.header('Access-Control-Allow-Origin', '*');
 //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -20,20 +17,8 @@ var proxy = new httpProxy.RoutingProxy();
 
 // app.use(allowCrossDomain);
 
-function apiProxy(host, port) {
-  return function(req, res, next) {
-    if(req.url.match(new RegExp('^\/api\/'))) {
-      proxy.proxyRequest(req, res, {host: host, port: port});
-    } else {
-      next();
-    }
-  }
-}
-
 var cors = require('cors');
 app.use(cors());
-
-app.use(apiProxy('https://api.binance.com', 8080));
 
 //Serve only static files from dist directory
 app.use(express.static(__dirname + '/dist'));
