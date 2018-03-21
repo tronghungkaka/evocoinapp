30c39b5c835a946d2924a7be3ad12f151d8fddfb
thangnh <thang.nguyenhuu91@gmail.com>
2018-03-21 11:41:11 +0700

    add header parameters

--- server.js
+++ server.js
@@ -19,6 +19,27 @@ function apiProxy(host, port) {
 var cors = require('cors');
 app.use(cors());
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://api.binance.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 app.use(apiProxy('localhost', 3000));
 
 //Serve only static files from dist directory
